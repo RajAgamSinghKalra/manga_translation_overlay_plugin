b00b40e7f087c910e94e73f35d2921bc0223920c
Raj Agam Singh Kalra <rajkagithub@gmail.com>
2025-11-23 22:11:17 +0530

    feat: configure CRX plugin with `defineManifest` and add `popup` as a rollup input.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { crx } from '@crxjs/vite-plugin'
+import { crx, defineManifest } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
+
+const manifestConfig = defineManifest(manifest)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    crx({ manifest }),
+    crx({ manifest: manifestConfig }),
   ],
   server: {
     port: 5173,
     strictPort: true,
     hmr: {
       port: 5173,
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        popup: 'index.html',
+      },
     },
   },
 })
